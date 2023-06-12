17f457be58d5fd9a58e6f9b9514d48a33c4fcfa2
Amir <haghdoostamir@gmail.com>
2023-06-12 23:40:49 +0330

    2nd session

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import Navbar from './components/Navbar';
+
 function App() {
-  return <div>salaam</div>;
+  return (
+    <div>
+      <Navbar></Navbar>
+      <div
+        style={{
+          display: 'flex',
+          border: '1px solid #444',
+          borderRadius: '5px',
+          overflow: 'hidden',
+          width:"400px",
+          justifyContent:"space-between"
+        }}
+      >
+        <div style={{display:"flex", gap:"20px", flexDirection:"column"}}>
+          <h3>کولر پنجره ای ال جی</h3>
+          <p>کارکرده</p>
+          <p>9500000 تومان</p>
+          <p>لحضاتی پیش در استادسرا</p>
+        </div>
+        <img style={{width:"200px", height:"200px"}} src="https://s100.divarcdn.com/static/thumbnails/1686608740/AZHqWCXa.webp" alt='کولر پنجره ای ال جی'/>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import './navbar.css';
+
+const Navbar = () => {
+  return (
+    <div>
+      <ul className='navbar-items'>
+        <li>خانه</li>
+        <li> جستجو</li>
+        <li>محصولات</li>
+        <li>سبد خرید</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
